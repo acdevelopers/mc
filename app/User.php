a4fc3f8595a302d1c13de589d86b3cc7d6f52ce3
Anitche Chisom <anitchec.dev@gmail.com>
2019-08-01 00:14:52 +0100

    Implement "MustVerifyEmail".

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use JamesMills\Uuid\HasUuidTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, HasUuidTrait;
 
