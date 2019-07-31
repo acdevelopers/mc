939724adcf2bdd2c238f6aca3099fd71a9517797
tsommie <anitchec.dev@gmail.com>
2019-07-31 22:58:18 +0100

    Configure uuid for user entity.

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use JamesMills\Uuid\HasUuidTrait;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasUuidTrait;
 
     /**
      * The attributes that are mass assignable.
